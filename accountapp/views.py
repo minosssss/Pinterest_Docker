f97b749d0bc086b2ea566a0ca9a23971082362b9
mh.lee <mh.lee239@gmail.com>
2021-10-20 22:49:27 +0900

    create update page

--- accountapp/views.py
+++ accountapp/views.py
@@ -6,10 +6,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
+from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
-
 
 def hello_world(request):
     if request.method == "POST":
@@ -22,8 +22,9 @@ def hello_world(request):
         return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
         hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
-
+        return render(request,
+                      'accountapp/hello_world.html',
+                      context={'hello_world_list': hello_world_list})
 
 class AccountCreateView(CreateView):
     model = User
@@ -33,8 +34,15 @@ class AccountCreateView(CreateView):
     template_name = 'accountapp/create.html'
     #성공했을 때 보여줄 페이지
 
-
 class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountUpdateForm #어떤 로그인 폼을 사용할 것인지 지정
+    success_url = reverse_lazy('accountapp:hello_world')
+    #성공하고 어디로 이동할 지 지정 # reverse는 함수형 base , reverse_lazy 객체형 base
+    template_name = 'accountapp/update.html'
+    #성공했을 때 보여줄 페이지
