4585501a2a8267c83b49f53dc13010be6a7b847b
mh.lee <mh.lee239@gmail.com>
2021-10-28 20:46:21 +0900

    add ownership

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http.response import HttpResponse, HttpResponseRedirect
@@ -6,25 +7,30 @@ from django.http import HttpResponse
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorator import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
+has_ownership = [login_required, account_ownership_required]
+
+@login_required
 def hello_world(request):
-    if request.method == "POST":
-        temp = request.POST.get('hello_world_input')
+        if request.method == "POST":
+            temp = request.POST.get('hello_world_input')
 
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
+            new_hello_world = HelloWorld()
+            new_hello_world.text = temp
+            new_hello_world.save()
 
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-    else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request,
-                      'accountapp/hello_world.html',
-                      context={'hello_world_list': hello_world_list})
+            return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        else:
+            hello_world_list = HelloWorld.objects.all()
+            return render(request,
+                          'accountapp/hello_world.html',
+                          context={'hello_world_list': hello_world_list})
 
 class AccountCreateView(CreateView):
     model = User
@@ -34,24 +40,24 @@ class AccountCreateView(CreateView):
     template_name = 'accountapp/create.html'
     #성공했을 때 보여줄 페이지
 
+
 class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
-    context_object_name = 'target_user'
-    form_class = AccountUpdateForm #어떤 로그인 폼을 사용할 것인지 지정
+    form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hello_world')
-    #성공하고 어디로 이동할 지 지정 # reverse는 함수형 base , reverse_lazy 객체형 base
     template_name = 'accountapp/update.html'
-    #성공했을 때 보여줄 페이지
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login')
-    #성공하고 어디로 이동할 지 지정 # reverse는 함수형 base , reverse_lazy 객체형 base
     template_name = 'accountapp/delete.html'
-    #성공했을 때 보여줄 페이지
