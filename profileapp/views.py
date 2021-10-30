0b193f17857c3b3978da605afbb049e5a5f3bd4a
mh.lee <mh.lee239@gmail.com>
2021-10-30 13:47:18 +0900

    profile page modified

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
@@ -9,12 +9,14 @@ from profileapp.decorator import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
-
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
 class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('accountapp:detail')
+    #detail로 가야 하지만, accoutapp의 url을 보면 pk를 받아야 하기에 get success_url 을 추가 해준다
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form): #제출을 눌렀을 때, 받는 form (image,nickname,message)
@@ -22,6 +24,11 @@ class ProfileCreateView(CreateView):
         temp_profile.user = self.request.user #임시유저를 보낸 유저로 저장
         temp_profile.save() #최종저장
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk':self.object.user.pk})
+        #self.object가 가르키는 것은 ProfileCreateView의 model(Profile)=User이다.
+        #그 user의 pk를 찾아서 넘겨줘라.
 
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
@@ -31,3 +38,6 @@ class ProfileUpdateView(UpdateView):
     form_class = ProfileCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk':self.object.user.pk})
