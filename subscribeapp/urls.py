cc34c2953df291575128052b1afc79346b2c12a6
mh.lee <mh.lee239@gmail.com>
2021-12-25 22:56:35 +0900

    Field Lookup (subscribe) 추가

--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from subscribeapp.views import SubscriptioinView
+from subscribeapp.views import SubscriptioinView, SubscriptionListView
 
 app_name = 'subscribeapp'
 
 urlpatterns = [
     path('subscribe/',SubscriptioinView.as_view(), name='subscribe'),
+    path('list/',SubscriptionListView.as_view(), name='list'),
 ]
