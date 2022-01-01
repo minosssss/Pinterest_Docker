8aab46695ecbbf1092c06dfd69317d50fd39d5d9
mh.lee <mh.lee239@gmail.com>
2022-01-01 14:25:57 +0900

    google material icons 추가 및 mypage 디테일 수정

--- accountapp/views.py
+++ accountapp/views.py
@@ -13,31 +13,14 @@ from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.decorator import account_ownership_required
 from accountapp.forms import AccountUpdateForm
-from accountapp.models import HelloWorld
 from articleapp.models import Article
 
 has_ownership = [login_required, account_ownership_required]
 
-@login_required
-def hello_world(request):
-        if request.method == "POST":
-            temp = request.POST.get('hello_world_input')
-
-            new_hello_world = HelloWorld()
-            new_hello_world.text = temp
-            new_hello_world.save()
-
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))
-        else:
-            hello_world_list = HelloWorld.objects.all()
-            return render(request,
-                          'accountapp/hello_world.html',
-                          context={'hello_world_list': hello_world_list})
-
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm #어떤 로그인 폼을 사용할 것인지 지정
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('home')
     #성공하고 어디로 이동할 지 지정 # reverse는 함수형 base , reverse_lazy 객체형 base
     template_name = 'accountapp/create.html'
     #성공했을 때 보여줄 페이지
@@ -61,7 +44,7 @@ class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountUpdateForm
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('home')
     template_name = 'accountapp/update.html'
 
 @method_decorator(has_ownership, 'get')
