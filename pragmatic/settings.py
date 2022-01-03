0126f7583f4e76ee199244863bf4d9148a93f5be
mh.lee <mh.lee239@gmail.com>
2022-01-03 21:35:06 +0900

    setting.py STATIC_ROOT add

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -142,6 +142,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
